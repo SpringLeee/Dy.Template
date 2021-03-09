f306f05eff3561eb66e5353332381d412f074bc9
Li Shuai <lishuai@diyibox.com>
2021-03-09 19:34:04 +0800

    commit

---
+++ templates/Dy.Template.Task/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Dy.Template.Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
